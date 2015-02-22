6952823efc1e1e35114679e2c6082ad0b843e661
x5and3r <x5ander.5am@gmail.com>
2015-02-23 01:54:10 +0200

    Update MainActivity.java

--- movieinformerjb/src/main/java/com/example/sanya/ua/movieinformerjb/MainActivity.java
+++ movieinformerjb/src/main/java/com/example/sanya/ua/movieinformerjb/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.sanya.ua.movieinformerjb;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -64,12 +63,8 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
