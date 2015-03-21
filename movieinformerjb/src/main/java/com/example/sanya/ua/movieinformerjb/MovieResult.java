37fe02ed7bd88fc0c066ddfc056810a34b23834d
x5and3r <x5ander.5am@gmail.com>
2015-03-21 16:13:34 +0200

    Update MovieResult.java

--- movieinformerjb/src/main/java/com/example/sanya/ua/movieinformerjb/MovieResult.java
+++ movieinformerjb/src/main/java/com/example/sanya/ua/movieinformerjb/MovieResult.java
@@ -1,98 +1,50 @@
 package com.example.sanya.ua.movieinformerjb;
 
 public class MovieResult {
-    private final String backdropPath;
-    private final String originalTitle;
-    private final int id;
-    private final double popularity;
-    private final String posterPath;
-    private final String releaseDate;
-    private final String title;
-    private final double voteAverage;
-    private final int voteCount;
+    private String backdropPath;
+    private String originalTitle;
+    private int id;
+    private double popularity;
+    private String posterPath;
+    private String releaseDate;
+    private String title;
+    private double voteAverage;
+    private int voteCount;
 
-    private MovieResult(Builder builder) {
-        backdropPath = builder.backdropPath;
-        originalTitle = builder.originalTitle;
-        id = builder.id;
-        popularity = builder.popularity;
-        posterPath = builder.posterPath;
-        releaseDate = builder.releaseDate;
-        title = builder.title;
-        voteAverage = builder.voteAverage;
-        voteCount = builder.voteCount;
-    }
-
-
-    public static class Builder {
-        private String backdropPath;
-        private String originalTitle;
-        private int id;
-        private double popularity;
-        private String posterPath;
-        private String releaseDate;
-        private String title;
-        private double voteAverage;
-        private  int voteCount;
-
-        public Builder(int id, String title) {
-            this.id = id;
-            this.title = title;
-        }
-
-        public Builder setBackdropPath(String backdropPath) {
+        public void setBackdropPath(String backdropPath) {
             this.backdropPath = "http://image.tmdb.org/t/p/w92" + backdropPath;
-            return this;
         }
 
-        public Builder setOriginalTitle(String originalTitle) {
+        public void setOriginalTitle(String originalTitle) {
             this.originalTitle = originalTitle;
-            return this;
         }
 
-        public Builder setId(int id) {
+        public void setId(int id) {
             this.id = id;
-            return this;
         }
 
-        public Builder setPopularity(double popularity) {
+        public void setPopularity(double popularity) {
             this.popularity = popularity;
-            return this;
         }
 
-        public Builder setPosterPath(String posterPath) {
+        public void setPosterPath(String posterPath) {
             this.posterPath = "http://image.tmdb.org/t/p/w154" + posterPath;
-            return this;
         }
 
-        public Builder setReleaseDate(String releaseDate) {
+        public void setReleaseDate(String releaseDate) {
             this.releaseDate = releaseDate;
-            return this;
         }
 
-        public Builder setTitle(String title) {
+        public void setTitle(String title) {
             this.title = title;
-            return this;
         }
 
-        public Builder setVoteAverage(double voteAverage) {
+        public void setVoteAverage(double voteAverage) {
             this.voteAverage = voteAverage;
-            return this;
         }
-        public Builder setVoteCount(int voteCount) {
+        public void setVoteCount(int voteCount) {
             this.voteCount = voteCount;
-            return this;
         }
-
-        public MovieResult build() {
-            return new MovieResult(this);
-        }
-
-    }
-
-    public static Builder newBuilder(int id, String title) {
-        return new Builder(id, title);
-    }
 
     public String getBackdropPath() {
         return backdropPath;
